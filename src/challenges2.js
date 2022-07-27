a8d68c6bcd5a7e973be021c6d646c1b07da29c92
Diogo Martins <me.martinsfe@gmail.com>
2022-07-27 12:02:27 -0300

    feat: resolve exercise n.11(bonus)

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,41 @@
+/* eslint-disable max-lines-per-function */
+/* eslint-disable complexity */
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arr) {
+  let generatedNumber = [];
+  let repeatedCounter = 0;
+
+  if (arr.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  for (let i = 1; i < arr.length; i += 1) {
+    for (let j = 0; j < i; j += 1) {
+      if (arr[i] === arr[j]) {
+        repeatedCounter += 1;
+      }
+    }
+  }
+
+  for (let num of arr) {
+    if (num < 0 || num > 9 || repeatedCounter > 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+
+  for (let i = 0; i < arr.length; i += 1) {
+    if (i === 0) {
+      generatedNumber += '(';
+    } else if (i === 2) {
+      generatedNumber += ') ';
+    } else if (i === 7) {
+      generatedNumber += '-';
+    }
+
+    generatedNumber += arr[i];
+  }
+
+  return generatedNumber;
 }
 
 // Desafio 12
